50f21940fd4e0a94d386afffde38219e40482070
Guillaume Bodson <bodson.guillaume@gmail.com>
2025-09-18 10:07:30 +0200

    Add PlannedWorkoutServiceCollectionExtensions class
    Introduce a new static class `PlannedWorkoutServiceCollectionExtensions` in the `WebUI.DI` namespace. This class includes the `AddPlannedWorkoutFactories` method, which registers multiple `IPlannedWorkoutCreator` implementations and an `IPlannedWorkoutFactory` with the service collection for dependency injection.

---
+++ WebUI/DI/PlannedWorkoutServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using WebUI.Creators;
+using WebUI.Services;
+using WebUI.Services.Interfaces;
+
+namespace WebUI.DI;
+
+public static class PlannedWorkoutServiceCollectionExtensions
+{
+    public static IServiceCollection AddPlannedWorkoutFactories(this IServiceCollection services)
+    {
+        services.AddScoped<IPlannedWorkoutCreator, EasyPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, SteadyPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, IntervalPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, TempoPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, LongRunPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, RacePlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutCreator, DefaultPlannedWorkoutCreator>();
+        services.AddScoped<IPlannedWorkoutFactory, PlannedWorkoutFactory>();
+        return services;
+    }
+}
