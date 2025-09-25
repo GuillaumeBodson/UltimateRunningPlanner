97c78b23e7ba082a8d9975b0477779142d0b7cf5
Guillaume Bodson <bodson.guillaume@gmail.com>
2025-09-25 15:09:39 +0200

    Add event creation and custom workout features
    - Updated `CalendarPage.razor` to include a new `CreateEventAsync` method for handling event creation via a dialog.
    - Enhanced `CustomWorkoutMapper.cs` with a `ToCustomWorkout` extension method for converting `CustomWorkoutModel` to `CustomWorkout`.
    - Updated `WebUI.csproj` to reference newer versions of `BlazorLibraries.MudPlanner` and `GarminRunerz.Workout.Services`.
    - Introduced `EventEditDialog.razor` for a user interface to create new workout events with validation.
    - Added `CustomWorkoutModel.cs` to define the structure for custom workouts.

--- WebUI/Mappers/CustomWorkoutMapper.cs
+++ WebUI/Mappers/CustomWorkoutMapper.cs
@@ -1,5 +1,7 @@
 ﻿using GarminRunerz.Workout.Services.Models;
 using System.Globalization;
+using WebUI.Models;
+using WebUI.Models.Workouts;
 
 namespace WebUI.Mappers
 {
@@ -22,6 +24,32 @@ namespace WebUI.Mappers
                 Pace = decimal.Parse(line[6], CultureInfo.InvariantCulture),
                 Speed = decimal.Parse(line[7], CultureInfo.InvariantCulture),
                 Description = line[8]
+            };
+        }
+
+        public static CustomWorkout ToCustomWorkout(this CustomWorkoutModel model, int weekNumber, Athlete athlete, int id)
+        {
+            var pace = model.RunType switch
+            {
+                RunType.Easy => athlete.EasyPace,
+                RunType.LongRun => athlete.MarathonPace,
+                RunType.Tempo => athlete.SemiMarathonPace,
+                RunType.Intervals => athlete.VmaPace,
+                RunType.Recovery => athlete.EasyPace,
+                RunType.Steady => athlete.SemiMarathonPace,
+                _ => athlete.EasyPace
+            };
+            return new CustomWorkout
+            {
+                WeekNumber = weekNumber,
+                RunType = model.RunType,
+                TotalDuration = model.TotalDuration,
+                Repetitions = model.Repetitions,
+                RunDuration = model.RunDuration,
+                CoolDownDuration = model.CoolDownDuration,
+                Description = model.Description,
+                Pace = (decimal)pace.ToMinutesDouble(),
+                Id = id
             };
         }
     }
---
+++ WebUI/Models/Workouts/CustomWorkoutModel.cs
@@ -0,0 +1,19 @@
+﻿using GarminRunerz.Workout.Services.Models;
+
+namespace WebUI.Models.Workouts;
+
+public class CustomWorkoutModel
+{
+    public RunType RunType { get; set; }
+
+    public int TotalDuration { get; set; }
+
+    public int Repetitions { get; set; }
+
+    public double RunDuration { get; set; }
+
+    public double CoolDownDuration { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+}
