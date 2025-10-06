58bc046534286d55cfaa42613deaa891397b7587
Guillaume Bodson <bodson.guillaume@gmail.com>
2025-10-06 11:47:24 +0200

    Add WorkoutPlanner.API project with models and API setup
    Introduced a new ASP.NET Core project, `WorkoutPlanner.API`, to the solution. Added `Planning` and `Workout` models to represent workout plans and sessions. Implemented a sample `WeatherForecastController` with an endpoint for weather forecasts. Configured the project with OpenAPI support, logging, and middleware for HTTPS redirection and authorization.
    
    Updated `Program.cs` to set up controllers and OpenAPI. Added `launchSettings.json` for development environment configuration and `appsettings.json` for runtime settings. Included dependencies such as `GarminRunerz.Workout.Services` and `Microsoft.EntityFrameworkCore`. Added an HTTP request file for testing the API.
    
    Updated the solution file to include the new project under the `backend` folder with Debug and Release build configurations.

---
+++ WorkoutPlanner.API/Models/Planning.cs
@@ -0,0 +1,12 @@
+﻿using GarminRunerz.Workout.Services.Models;
+
+namespace WorkoutPlanner.API.Models;
+
+public class Planning
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public List<Workout> Workouts { get; set; } = [];
+    public int AthleteId { get; set; }
+    public HashSet<Dictionary<DayOfWeek, RunType?>> TrainingTemplates { get; set; } = [];
+}
---
+++ WorkoutPlanner.API/Models/Workout.cs
@@ -0,0 +1,22 @@
+﻿using GarminRunerz.Workout.Services.Models;
+
+namespace WorkoutPlanner.API.Models;
+
+public class Workout
+{
+    public int Id { get; set; }
+    public int ComputedId { get; set; }
+    public int WeekNumber { get; set; }
+    public RunType RunType { get; set; }
+    public int TotalDuration { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public int EstimatedDistance { get; set; }
+    public int EstimatedTime { get; set; }
+    public decimal Pace { get; set; }
+    public string Name { get; set; } = string.Empty;
+    protected string EventLabel { get; set; } = string.Empty;
+
+    public int PlanningId { get; set; }
+    public Planning Planning { get; set; } = null!;
+}
